c7ad94e34d6c409e89426ddc0100363be1ed583a
Medium-Dragon <66219759+Medium-Dragon@users.noreply.github.com>
2020-06-01 11:34:30 -0700

    Pushing for testing

--- main.py
+++ main.py
@@ -18,26 +18,30 @@ async def get_current_consumption(switch: configparser, q: asyncio.Queue) -> Non
         await q.put((switch['mac'], sp.current_consumption, t))
         if time.time() < (t + 60.0 ): 
             await asyncio.sleep(60.0 - (t % 60) )
-            print 
+            print (switch['mac'] + " sleeping for " + str(60.0 - (t % 60) ))
     return
 
 async def log_to_influxdb( q: asyncio.Queue ) -> None:
     while True:
+        await asyncio.sleep(random.choice([2, 3, 5, 7, 11, 13, 17, 19, 23]))
         mac, current_consumption, t = await q.get()
         print(str(mac) + " " + str(current_consumption) + " " + str(t))
     return
 
 async def main():
+    random.seed(444)
+
     switchcfg = configparser.ConfigParser()
     switchcfg.read('switches.cfg')
+
     q = asyncio.Queue()
     producers = [asyncio.create_task( get_current_consumption(switchcfg[switch], q) ) for switch in switchcfg.sections() ] 
     consumers = [asyncio.create_task( log_to_influxdb( q ) ) for switch in switchcfg.sections() ]
+
     await asyncio.gather(*producers)
     await q.join()
     for c in consumers:
         c.cancel()
-
 
 asyncio.run(main())
 
