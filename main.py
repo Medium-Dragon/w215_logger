4720a87d74bc0dff82e01ed214c6827eceeb0a8a
Medium-Dragon <66219759+Medium-Dragon@users.noreply.github.com>
2020-06-02 11:30:23 -0700

    Added av time to sleep calc

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
 #!python3
-#configparser
-#influxdb
-#pyW215
+#pip install the following:
+#   configparser
+#   influxdb
+#   pyW215
 import asyncio
 import configparser
 from pyW215.pyW215 import SmartPlug, ON, OFF
 from influxdb import InfluxDBClient
-import itertools as it
 import os
 import random
 import time
+import math
+import statistics
 
-async def get_current_consumption(switch: configparser, q: asyncio.Queue) -> None:
+
+async def get_current_consumption(switch: configparser,
+                                  q: asyncio.Queue) -> None:
     while True:
-        t = time.time()
-        sp = SmartPlug(switch['ip'], switch['pin'],use_legacy_protocol=True)
-        await q.put((switch['mac'], sp.current_consumption, t))
-        if time.time() < (t + 60.0 ): 
-            await asyncio.sleep(60.0 - (t % 60) )
-            print (switch['mac'] + " sleeping for " + str(60.0 - (t % 60) ))
+        t1 = time.time()
+        sp = SmartPlug(switch['ip'], switch['pin'], use_legacy_protocol=True)
+        t2 = time.time()
+        t = int(math.floor(statistics.mean([t1, t2])))
+        await q.put((str(switch['mac']), str(sp.current_consumption), int(t)))
+        if int(math.ceil(time.time())) < (t + 60):
+            t = 60 - (t % 60)
+            await asyncio.sleep(t)
+            print(switch['mac'] + " sleeping for " + str(t))
     return
 
-async def log_to_influxdb( q: asyncio.Queue ) -> None:
+
+async def log_to_influxdb(q: asyncio.Queue) -> None:
     while True:
-        await asyncio.sleep(random.choice([2, 3, 5, 7, 11, 13, 17, 19, 23]))
+        await asyncio.sleep(random.choice([17, 19, 23]))
         mac, current_consumption, t = await q.get()
         print(str(mac) + " " + str(current_consumption) + " " + str(t))
     return
+
 
 async def main():
     random.seed(444)
@@ -35,13 +44,19 @@ async def main():
     switchcfg.read('switches.cfg')
 
     q = asyncio.Queue()
-    producers = [asyncio.create_task( get_current_consumption(switchcfg[switch], q) ) for switch in switchcfg.sections() ] 
-    consumers = [asyncio.create_task( log_to_influxdb( q ) ) for switch in switchcfg.sections() ]
+    producers = [
+        asyncio.create_task(get_current_consumption(switchcfg[switch], q))
+        for switch in switchcfg.sections()
+    ]
+    consumers = [
+        asyncio.create_task(log_to_influxdb(q))
+        for switch in switchcfg.sections()
+    ]
 
     await asyncio.gather(*producers)
     await q.join()
     for c in consumers:
         c.cancel()
 
-asyncio.run(main())
 
+asyncio.run(main())
